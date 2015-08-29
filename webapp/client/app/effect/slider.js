5bddcf067b9042ff1e5b0e62f8d0802253f2ac6e
richardgong <909253305@qq.com>
2015-08-29 21:28:47 +0800

    submit

--- webapp/client/app/effect/slider.js
+++ webapp/client/app/effect/slider.js
@@ -1,3 +1,15 @@
 /**
  * Created by richard on 2015/8/29.
  */
+function slider(){
+    this.index = 0;
+    this.direction = ['left','right'];
+}
+slider.prototype= {
+    constructor:slider,
+    moves:function(){},
+    moveLeft: function(){},
+    moveRight:function(){
+
+    }
+}
