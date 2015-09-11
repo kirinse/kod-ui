214b06355909a2cf8f7feb0e9b7e3c0628cfb30a
richardgong <909253305@qq.com>
2015-09-11 20:46:48 +0800

    submit

--- push.sh
+++ push.sh
@@ -2,6 +2,17 @@ git add .
 git commit -m"submit"
 git push origin  develop
 
+soucePath=webapp/client
+showPath=../frontend.github.io;
+cp $soucePath $showPath;
+cd $showPath;
+
+git add .
+git commit -m"submit"
+git push origin  develop
+
+
+
 
 
 
